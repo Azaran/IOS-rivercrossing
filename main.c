a831c8f3a4428ff4b5851cafa4a0157a20319704
Vojtech Vecera <vecera.vojta@seznam.cz>
2014-04-26 09:51:32 +0200

    libraries added

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * Projekt: IOS, projekt 2, synchronizace procesu  
+ * Autor: Vojtech Vecera, xvecer18@stud.fit.vutbr.cz
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <semaphores.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    
+    
+    
+    
+}
